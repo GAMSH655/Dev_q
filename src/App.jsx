f59740649c5eaa04be53b1e76524d5b88e5bb030
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-04-25 12:27:40 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import Header from '../src/Components/Header'
+function App() {
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Header= () => {
+  return (
+    <div>
+       <h3 className="logoText">
+         jjjj
+       </h3>
+    </div>
+  )
+}
+
+export default Header;
