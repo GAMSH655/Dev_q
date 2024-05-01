e003d2e7671f6cf2d3c83609077c18cd8772ab0a
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-05-01 06:46:28 -0700

    working in the connect components

---
+++ src/Components/Connect.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import "./Connect.css"
+const Connect = () => {
+  return (
+    <div className='ContParentContainer'>
+        <div className="contactCont">
+            <h3 className="let">Let’s connect</h3>
+            <h className="hello">
+                Say hello shittuabdquadri0@gmail.com
+            </h>
+            <div className="iconDiv">
+            <a href="https://github.com/GAMSH655"> <i className="fa-brands fa-github  fa-2xl contact-icon"></i></a>
+            <a href="https://www.linkedin.com/in/abdulquadri-shittu-59480b262/"> <i className="fa-brands fa-linkedin  fa-2xl contact-icon"></i></a>
+            <a href="https://twitter.com/BIG_DEV360"> <i class="fa-brands fa-x-twitter fa-2xl contact-icon"></i></a>
+            </div>
+        </div>
+         <form action="">
+            <div className="nameInput">
+                <h3 className="inputname">name</h3>
+                <input type="text" className='input' />
+            </div>
+
+            <div className="nameInput">
+                <h3 className="inputname">name</h3>
+                <input type="mail" className='input' />
+            </div>
+
+            <div className="nameInput">
+                <h3 className="inputname">name</h3>
+                <input type="text" className='input' />
+            </div>
+            <textarea placeholder='enter your message'></textarea>
+         </form>
+    </div>
+  )
+}
+
+export default Connect
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../Components/Hero'
 import Header from "../Components/Header"
 import Project from "../Components/Project"
+import Connect from "../Components/Connect"
 const Home = () => {
   return (
     <div>
         <Header/>
         <Hero/>
         <Project/>
+        <Connect/>
     </div>
   )
 }
