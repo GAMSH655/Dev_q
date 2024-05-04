fde54c795d0752e7eae6b65c4f597ea52c8fde49
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-05-03 17:17:06 -0700

    working

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Project.css"
 import Ecom from "../assets/ecom.png"
 import Crypto from "../assets/Crypto.png"
-import Star from "../Components/Star"
+import Krib from "../assets/Krib.png"
 const Project = () => {
 
   return (
@@ -22,7 +22,7 @@ const Project = () => {
                  Exclusive is an e-commerce website that is built purely on Reactjs , below are features implemented in the project.
                 </p>
                 <p  className='pInf'> 📌Implemented star  rating functionallity  which allows users to rate the goods.</p>
-                <p className='pInf'> 📌Users can easily add to cart and delete from cart and keep track of the goods using a react hook called useContext.</p>
+                <p className='pInf'> 📌Users can easily add to cart and delete from cart and keep track of the goods , by  implementing a react hook called useContext.</p>
                 <p className='pInf'> 📌 Users can easily route from page to another using implementing a react properties called react-router.</p>
                 <div className="bottomInfo">
                   <div className="linkCont">
@@ -52,7 +52,7 @@ const Project = () => {
              </div>
          </div>
          <div className="projBox1">
-             <img src={Ecom} alt=""  className='projImg' draggable="false"/>
+             <img src={Krib} alt=""  className='projImg' draggable="false"/>
              <div className="projTextBox">
                 <h3 className="projHeader">
                     e-commerce
@@ -61,16 +61,16 @@ const Project = () => {
                 amed up with a designer to breathe life into a promotional webpage for our beloved show, Adventure Time. Delivered a fully responsive design with dynamic content capabilities, seamlessly integrating a newsletter feature to keep fans updated with the latest adventures.
                 </p>
                 <div className="bottomInfo">
-                  <div className="linkCont">
-                     <a href="https://e-commerce-nine-delta-43.vercel.app/" className='prjLink'> live view  <i className="fa-solid fa-link icon"></i></a>
+                <div className="linkCont">
+                     <a href="https://e-commerce-nine-delta-43.vercel.app/" className='prjLink'> live view  <i class="fa-solid fa-link icon"></i></a>
 
                      <a href="https://github.com/GAMSH655/E-commerce.git" className='prjLink'>repo link <i className="fa-brands fa-github icon"></i></a>
-                  </div>
+                  </div>ss
               </div>
              </div>
          </div>
          <div className="projBox1">
-             <img src={Ecom} alt=""  className='projImg' draggable="false"/>
+             {/* <img src={Krib} alt=""  className='projImg' draggable="false"/> */}
              <div className="projTextBox">
                 <h3 className="projHeader">
                     e-commerce
