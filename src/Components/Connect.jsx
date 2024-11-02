3d71e31c9dd214e8858b06fca21378000f01e975
BIG_DEV360 <shittuabdquadri0@gmail.com>
2024-11-01 22:38:53 -0700

    refactoring the code

--- src/Components/Connect.jsx
+++ src/Components/Connect.jsx
@@ -2,38 +2,45 @@ import React from 'react'
 import "./Connect.css"
 const Connect = () => {
   return (
-    <div className='ContParentContainer'>
-        <div className="contactCont">
-            <h3 className="let">Let’s connect</h3>
-            <h3 className="hello">
-                Say hello shittuabdquadri0@gmail.com
-            </h3>
-            <div className="iconDiv">
-            <a href="https://github.com/GAMSH655" class='contactLink'> <i className="fa-brands fa-github  fa-2xl contact-icon"></i></a>
-            <a href="https://www.linkedin.com/in/abdulquadri-shittu-59480b262/" className='contactLink'> <i className="fa-brands fa-linkedin  fa-2xl contact-icon"></i></a>
-            <a href="https://twitter.com/BIG_DEV360" className='contactLink'> <i className="fa-brands fa-x-twitter fa-2xl contact-icon"></i></a>
-            <
-                a href="https://mail.google.com/mail/u/0/?hl=sv#inbox?compose=new" class='contactLink'> <i className="fa-brands fa-x-twitter fa-2xl contact-icon"></i></a>
-            </div>
-        </div>
-         <form action="">
-            <div className="nameInput">
-                <h3 className="inputname">name</h3>
-                <input type="text" className='input' />
-            </div>
-
-            <div className="nameInput">
-                <h3 className="inputname">email</h3>
-                <input type="mail" className='input' />
-            </div>
-
-            <div className="nameInput">
-                <h3 className="inputname">number</h3>
-                <input type="number" className='input' />
-            </div>
-            <textarea placeholder='enter your message'></textarea>
-         </form>
+    <div class="ContParentContainer">
+    <div class="contactCont">
+      <h3 class="let">Let’s connect</h3>
+      <h3 class="hello">
+        Say hello shittuabdquadri0@gmail.com
+      </h3>
+      <div class="iconDiv">
+        <a href="https://github.com/GAMSH655" class="contactLink">
+          <i className="fa-brands fa-github fa-2xl contact-icon"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/abdulquadri-shittu-59480b262/" class="contactLink">
+          <i className="fa-brands fa-linkedin fa-2xl contact-icon"></i>
+        </a>
+        <a href="https://twitter.com/BIG_DEV360" class="contactLink">
+          <i className="fa-brands fa-x-twitter fa-2xl contact-icon"></i>
+        </a>
+        <a href="https://mail.google.com/mail/u/0/?hl=sv#inbox?compose=new" class="contactLink">
+        <i className="fa-solid fa-envelope fa-2xl contact-icon"></i>
+        </a>
+      </div>
     </div>
+  
+    <form action="">
+      <div class="nameInput">
+        <h3 className="inputname">Name</h3>
+        <input type="text" class="input" />
+      </div>
+      <div className="nameInput">
+        <h3 className="inputname">Email</h3>
+        <input type="email" className="input" />
+      </div>
+      <div className="nameInput">
+        <h3 className="inputname">Number</h3>
+        <input type="number" class="input" />
+      </div>
+      <textarea placeholder="Enter your message" className="messageInput"></textarea>
+    </form>
+  </div>
+  
   )
 }
 
